489ad542af80343f2eadb666779f6c2a1a92c3dc
mleda <efreun1@wgu.edi>
2024-10-27 20:36:28 -0500

    sales person class and data

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+//create an array of objects
 
+salesPersonList: SalesPerson[] = [
+  new SalesPerson("Anup", "Kumar", "anup.kumar@gmail.com", 5000),
+  new SalesPerson("John", "Doe", "john.doe@gmail.com", 7000),
+  new SalesPerson("Gary", "Oh", "gary.oh@gmail.com", 4000),
+  new SalesPerson("Leah", "Connor", "leah.connor@gmail.com", 9000)
+];
   constructor() { }
 
   ngOnInit(): void {
